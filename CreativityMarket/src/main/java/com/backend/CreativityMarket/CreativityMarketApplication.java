e0107826a27c0321ccf84eae3d7d5fd3884ee9ac
SB <ijhollaw@uncg.edu>
2026-01-22 17:37:52 -0500

    updated project
    added file structure, most dependencies

---
+++ CreativityMarket/src/main/java/com/backend/CreativityMarket/CreativityMarketApplication.java
@@ -0,0 +1,13 @@
+package com.backend.CreativityMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativityMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativityMarketApplication.class, args);
+	}
+
+}
---
+++ CreativityMarket/src/test/java/com/backend/CreativityMarket/CreativityMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.CreativityMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativityMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
