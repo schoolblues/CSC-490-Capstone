ab2de83761e6a8fd61209a36bb25107b6890841c
SB <ijhollaw@uncg.edu>
2026-03-18 18:19:39 -0400

    Update AssetRepository.java
    updated repository

--- CreativityMarket/src/main/java/com/backend/CreativityMarket/User/AssetRepository.java
+++ CreativityMarket/src/main/java/com/backend/CreativityMarket/User/AssetRepository.java
@@ -2,7 +2,11 @@ package com.backend.CreativityMarket.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-//TODO: placeholder
+
+import java.util.List;
+
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+    List<Asset> findByCategory(String category);
+    List<Asset> findByCreatorName(String creatorName);
 }
